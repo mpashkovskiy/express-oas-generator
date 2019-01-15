d6f31001f9013aa78c1c6b6929239257540a1283
Eyal Zamir <Eyal.Zamir@carlsonwagonlit.com>
2019-01-15 11:17:01 +0200

    fixes after comments

--- test/index_tests.js
+++ test/index_tests.js
@@ -208,8 +208,8 @@ describe('index.js', () => {
 
 });
 
-it('WHEN package json includes baseUrlPath THEN spec description is updated', done => {
-  const path = '/hello';
+it('WHEN package json includes baseUrlPath THEN spec description is updated', () => {
+  //const path = '/hello';
   const newTitle = 'New title';
   const app = express();
   const pkgJsonPath = 'test/specs/withBaseUrlPath';
@@ -219,30 +219,15 @@ it('WHEN package json includes baseUrlPath THEN spec description is updated', do
     return spec;
   });
 
-  app.get(path, (req, res, next) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(PLAIN_TEXT_RESPONSE);
-    return next();
-  });
-  app.set('port', port);
-
-  const server = app.listen(app.get('port'), function() {
-    setTimeout(() => {
-      request.get(`http://localhost:${port}${path}?a=1`, () => {
-        const spec = generator.getSpec();
-        assert.equal(spec.info.description.indexOf(', base url :') > 0, true);
-        server.close();
-        done();
-      });
-    }, MS_TO_STARTUP);
-  });
-
+  setTimeout(() => {
+    const spec = generator.getSpec();
+    assert.equal(spec.info.description.indexOf(', base url :') > 0, true);
+  }, 1001);
 
 });
 
-it('WHEN package json does notinclude' +
-  ' baseUrlPath THEN spec description is not updated', done => {
-  const path = '/hello';
+
+it('WHEN package json does not include baseUrlPath THEN spec description is not updated', () => {
   const newTitle = 'New title';
   const app = express();
   const pkgJsonPath = 'test/specs/withoutBaseUrlPath';
@@ -252,23 +237,10 @@ it('WHEN package json does notinclude' +
     return spec;
   });
 
-  app.get(path, (req, res, next) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(PLAIN_TEXT_RESPONSE);
-    return next();
-  });
-  app.set('port', port);
-
-  const server = app.listen(app.get('port'), function() {
-    setTimeout(() => {
-      request.get(`http://localhost:${port}${path}?a=1`, () => {
-        const spec = generator.getSpec();
-        assert.equal(spec.info.description.indexOf(', base url :') > 0, false);
-        server.close();
-        done();
-      });
-    }, MS_TO_STARTUP);
-  });
+  setTimeout(() => {
+    const spec = generator.getSpec();
+    assert.equal(spec.info.description.indexOf(', base url :') > 0, false);
+  }, 1001);
 
 
 });
