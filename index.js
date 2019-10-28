3ef933edf3085afda264fd56fba54a1fdc75a0b4
Kipras Melnikovas <kipras@kipras.org>
2019-10-28 16:14:30 +0200

    Extract middleware injection into separate functions & export
    I've also extensively documented the type definitions with jsdoc.
    
    This is NOT a breaking change.
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- index.js
+++ index.js
@@ -156,54 +156,169 @@ function updateSchemesAndHost(req) {
   }
 }
 
-module.exports.init = (aApp, aPredefinedSpec, aPath, aWriteInterval, aApiDocsPath = 'api-docs') => {
-  app = aApp;
-  predefinedSpec = aPredefinedSpec;
-  const writeInterval = aWriteInterval || 10 * 1000;
+/** TODO - type defs for Express don't work (I tried @external) */
+/**
+ * Apply this **first**!
+ *
+ * (straight after creating the express app (as the very first middleware))
+ *
+ * @description `response` middleware.
+ *
+ * @param {Express} expressApp - the express app
+ *
+ * @param {Object} [options] optional configuration options
+ * @param {string|undefined} [options.pathToOutputFile=undefined] where to write the openAPI specification to.
+ * Specify this to create the openAPI specification file.
+ * @param {number} [options.writeIntervalMs=10000] how often to write the openAPI specification to file
+ *
+ * @returns void
+ */
+function injectResponseMiddleware(expressApp, options = { pathToOutputFile: undefined, writeIntervalMs: 1000 * 10 }) {
+  /**
+   * save the `expressApp` to our local `app` variable.
+   * Used here, but not in `injestRequestMiddleware`,
+   * because this comes before it.
+   */
+  app = expressApp;
 
-  // middleware to handle responses
+  const { pathToOutputFile, writeIntervalMs } = options;
+
+  /** middleware to handle RESPONSES */
+  // eslint-disable-next-line complexity
   app.use((req, res, next) => {
     try {
       const methodAndPathKey = getMethod(req);
+
       if (methodAndPathKey && methodAndPathKey.method) {
         processors.processResponse(res, methodAndPathKey.method);
       }
-      let firstTime = true;
+
+      let firstTime = true; /** run instantly the first time. TODO do not set set `lastRecordTime` at the start until we run */
       const ts = new Date().getTime();
-      if (firstTime || aPath && ts - lastRecordTime > writeInterval) {
+
+      if (firstTime || pathToOutputFile && ts - lastRecordTime > writeIntervalMs) {
         firstTime = false;
         lastRecordTime = ts;
-        fs.writeFile(aPath, JSON.stringify(spec, null, 2), 'utf8', err => {
-          const fullPath = path.resolve(aPath);
+
+        fs.writeFile(pathToOutputFile, JSON.stringify(spec, null, 2), 'utf8', err => {
+          const fullPath = path.resolve(pathToOutputFile);
+
           if (err) {
             throw new Error(`Cannot store the specification into ${fullPath} because of ${err.message}`);
           }
         });
       }
-    } catch (e) {}
-    next();
-  });
-
-  // make sure we list routes after they are configured
-  setTimeout(() => {
-    // middleware to handle requests
-    app.use((req, res, next) => {
-      try {
-        const methodAndPathKey = getMethod(req);
-        if (methodAndPathKey && methodAndPathKey.method && methodAndPathKey.pathKey) {
-          const method = methodAndPathKey.method;
-          updateSchemesAndHost(req);
-          processors.processPath(req, method, methodAndPathKey.pathKey);
-          processors.processHeaders(req, method, spec);
-          processors.processBody(req, method);
-          processors.processQuery(req, method);
-        }
-      } catch (e) {}
+    } catch (e) {
+      /** TODO - shouldn't we do something here? */
+    } finally {
+      /** always call the next middleware */
       next();
-    });
+    }
+  });
+}
+
+module.exports.injectResponseMiddleware = injectResponseMiddleware;
+
+/** TODO - type defs for Express don't work (I tried @external) */
+/**
+ * Apply this **last**!
+ *
+ * (as the very last middleware of your express app)
+ *
+ * @description `request` middleware.
+ * Applies to the `app` you provided in `injectResponseMiddleware`
+ *
+ * @returns void
+ */
+function injectRequestMiddleware() {
+  /** middleware to handle REQUESTS */
+  // eslint-disable-next-line complexity
+  app.use((req, res, next) => {
+    try {
+      const methodAndPathKey = getMethod(req);
+      if (methodAndPathKey && methodAndPathKey.method && methodAndPathKey.pathKey) {
+        const method = methodAndPathKey.method;
+        updateSchemesAndHost(req);
+        processors.processPath(req, method, methodAndPathKey.pathKey);
+        processors.processHeaders(req, method, spec);
+        processors.processBody(req, method);
+        processors.processQuery(req, method);
+      }
+    } catch (e) {
+      /** TODO - shouldn't we do something here? */
+    } finally {
+      next();
+    }
+  });
+}
+
+module.exports.injectRequestMiddleware = injectRequestMiddleware;
+
+/**
+ * TODO
+ *
+ * 1. Rename the parameter names
+ * (this will require better global variable naming to allow re-assigning properly)
+ *
+ * 2. the `aPath` is ignored only if it's `undefined`,
+ * but if it's set to an empty string `''`, then the tests fail.
+ * I think we should be checking for falsy values, not only `undefined` ones.
+ *
+ * 3. (Breaking) Use object for optional parameters:
+ * https://github.com/mpashkovskiy/express-oas-generator/issues/35
+ *
+ */
+/**
+ * @warn it's preferred that you use `injectResponseMiddleware`,
+ * `injectRequestMiddleware` and `serveApiDocs` **individually**
+ * and not directly from this `init` function,
+ * because we need `injectRequestMiddleware` to be placed as the
+ * very last middleware and we cannot guarantee this here,
+ * since we're only using an arbitrary setTimeout of `1000` ms.
+ *
+ * See
+ * https://github.com/mpashkovskiy/express-oas-generator/pull/32#issuecomment-546807216
+ *
+ * @description initialize the `express-oas-generator`.
+ *
+ * This will apply both `injectResponseMiddleware` and `injectRequestMiddleware`
+ * and also will call `serveApiDocs`.
+ *
+ * @param {Express} aApp - the express app
+ * @param {*} [aPredefinedSpec=undefined]
+ * @param {string|undefined} [aPath=undefined] where to write the openAPI specification to.
+ * Specify this to create the openAPI specification file.
+ * @param {number} [aWriteInterval=10000] how often to write the openAPI specification to file
+ * @param {string} [aApiDocsPath=api-docs] where to serve the openAPI docs. Defaults to `api-docs`
+ */
+function init(aApp, aPredefinedSpec = undefined, aPath = undefined, aWriteInterval = 1000 * 10, aApiDocsPath = 'api-docs') {
+  /**
+   * TODO - shouldn't `predefinedSpec` be assigned @ `serveApiDocs`?
+   *
+   * I don't know if the `predefinedSpec` is used anywhere for `injectResponseMiddleware`
+   * and before `injectRequestMiddleware` is called
+   */
+  predefinedSpec = aPredefinedSpec;
+
+  injectResponseMiddleware(aApp, { pathToOutputFile: aPath, writeIntervalMs: aWriteInterval });
+
+  /**
+   * make sure we list routes after they are configured
+   *
+   * TODO - this (setTimeout) is error-prone.
+   * See https://github.com/mpashkovskiy/express-oas-generator/pull/32#issuecomment-546807216
+   *
+   * There could be some heavylifing initialization that takes
+   * more than a second and in those cases the requests processing middleware
+   * wouldn't be the last one.
+   */
+  setTimeout(() => {
+    injectRequestMiddleware();
     serveApiDocs({ path: aApiDocsPath, predefinedSpec });
   }, 1000);
-};
+}
+
+module.exports.init = init;
 
 module.exports.getSpec = () => {
   return patchSpec(predefinedSpec);
