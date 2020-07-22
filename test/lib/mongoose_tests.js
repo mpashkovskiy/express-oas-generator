7a47b38a5f361b1adf04d9c0da2663511d667ebf
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-07-22 20:40:59 -0300

    Helper to generate mongoose models spec

---
+++ test/lib/mongoose_tests.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {generateMongooseModelsSpec} = require('../../lib/mongoose.js');
+require('./mongoose_models/student');
+
+describe('mongoose.js', () => {
+
+  describe('generateMongooseModelsSpec()', () => {
+    it('WHEN supplied mongoose model does not exist THEN should throw exception', () => {
+      const mongooseModels = ['User', 'Student'];
+    
+      try {
+        generateMongooseModelsSpec(mongooseModels);
+        throw new Error();
+      } catch (err) {
+        expect(err.message).toContain(`Schema hasn't been registered for model "${mongooseModels.shift()}"`);
+      }
+    });
+
+    it('WHEN supplied mongoose model does not exist THEN should throw exception', () => {
+      const mongooseModels = ['Student'];
+    
+      const mongooseModelSpecs = generateMongooseModelsSpec(mongooseModels);
+      const studentModelSpec = mongooseModelSpecs[mongooseModels.shift()];
+      /* Not asserting the spec itself, trusting mongoose-to-swagger :) */
+      expect(studentModelSpec).toBeDefined();
+    });
+  });
+});
